729fb0038e59e9f1570c3b4e5f322b2783299f31
Harish <harisiva.fb@gmail.com>
2024-05-19 20:15:37 +0530

    change

--- Project/tests/test_calculator.py
+++ Project/tests/test_calculator.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the parent directory to the sys.path to import calculator module
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'Project')))
+
 import calculator
 
 def test_add():
